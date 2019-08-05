227fa8f3009a6b7fd86d7d8fdca58594b3e38349
yikeshu7351 <40238619+yikeshu7351@users.noreply.github.com>
2019-08-06 00:17:21 +0800

    Update MainSerer.java

--- test/src/main/java/com/server/MainSerer.java
+++ test/src/main/java/com/server/MainSerer.java
@@ -2,6 +2,8 @@ package com.server;
 
 public class MainSerer {
 	public static void main(String[] args) {
+		System.out.println("hello github before");
 		System.out.println("hello github");
+		System.out.println("hello github after");
 	}
 }
