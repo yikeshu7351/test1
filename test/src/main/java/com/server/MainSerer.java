0877071227783ca77346722470336997107eca42
yikeshu7351 <hxyikeshu@163.com>
2019-08-06 00:50:22 +0800

    解决冲突

--- test/src/main/java/com/server/MainSerer.java
+++ test/src/main/java/com/server/MainSerer.java
@@ -2,6 +2,9 @@ package com.server;
 
 public class MainSerer {
 	public static void main(String[] args) {
+		System.out.println("hello github before");
 		System.out.println("hello github");
+		System.out.println("hello github very good");
+		System.out.println("hello github after");
 	}
 }
