29843e7270febabc923bad1dc7f0591a44cbfa93
yikeshu7351 <40238619+yikeshu7351@users.noreply.github.com>
2019-08-07 00:00:30 +0800

    Create MerSkuPorper.java

---
+++ test/src/main/java/com/controller/MerSkuPorper.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+public class MerSkuPorper {
+	public String addSkuPro(String name) {
+		return "add" + name + "suc";
+	}
+	public String delSkuPro(String name) {
+		return "del" + name + "suc";
+	}
+	public String updateSkuPro(String name) {
+		return "update" + name + "suc";
+	}
+	public String searchSkuPro(String name) {
+		return "search" + name + "suc";
+	}
+	public String searchSkuPro(String name) {
+		return "search" + name + "suc v2";
+	}
+}
